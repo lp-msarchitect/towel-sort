b152f92c27959b077c3de8a16da7eb934034872a
Leonid Petrov <lp.msarchitect@gmail.com>
2020-03-22 17:22:28 +0300

    feat: Add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix = null) {
+    if (matrix == null) {
+        return [];
+    }
+
+    matrix.forEach((arr, i) => {
+        if (i % 2 == 0) {
+            arr.sort((a, b) => {
+                if (a < b) return -1;
+                if (a == b) return 0;
+                if (a > b) return 1;
+            });
+        } else {
+            arr.sort((a, b) => {
+                if (a < b) return 1;
+                if (a == b) return 0;
+                if (a > b) return -1;
+            });
+        }
+    });
+    return matrix.reduce((result, item) => {
+        return [].concat(result, item);
+    }, []);
+};
